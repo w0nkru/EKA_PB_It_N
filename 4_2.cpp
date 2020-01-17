e3ae874bc71e5a9502e550ed727518744a6a3fd9
w0nkru <57651257+w0nkru@users.noreply.github.com>
2020-01-17 05:03:22 +0200

    Update 4_2.cpp

--- 4_2.cpp
+++ 4_2.cpp
@@ -4,53 +4,68 @@
 #include <string>
 using namespace std;
 
-const int MAX_TRIES=10;
+const int gameover=10;
+int points = 0;
 int letterFill (char, string, string&);
+
 int main ()
 {
-string name;
-char letter;
-int guesses=0;
-string word;
-string words[] = {"riga", "kuldiga", "jurmala", "liepaja", "daugavpils", "kandava", "ventspils", "pure", "cesis", "rezekne"};
+    string name;
+    char letter;
+    int punkti=0;
+    string word;
+    string words[] = {"riga", "daugavpils", "liepaja", "jelgava", "jurmala", "ventspils", "rezekne", "valmiera",
+                                             "ogre", "jekabpils", "salaspils", "tukums", "cesis", "sigulda",
+                                             "olaine", "kuldiga", "saldus", "talsi", "dobele"};
 
-int n=rand()% 10;
-word=words[n];
+    time_t t;
+    srand((unsigned) time(&t));
+    
+    for(int j = 0; j < 1;){
+    int n=rand()% 20;
+    word=words[n];
 
-string unknown(word.length(),'*'); //aizpildam vārdu ar *
+    string unknown(word.length(),'*'); //aizpildam vārdu ar *
 
         cout << "Word guessing game!\n";
         cout << "Each * represents letter count in word.\n";
         cout << "Type 1 letter by guess.\n";
-        cout << "You have " << MAX_TRIES << " tries.\n";
+        cout << "For each wrong guess You get 1 point. 10 points and your game ends.\n";
+        cout << "You have " << punkti << "/10 points.\n";
         cout << "----------------------------------------";
 
-    while (guesses < MAX_TRIES)
+    while (punkti < gameover)
     {
         cout << "\n\n" << unknown;
-        cout << "\n\nGuess a letter: ";
+        cout << "\nGuess a letter: ";
         cin >> letter;
 // Ja pareizs, tad * aizvieto ar burtu vai kļūdas gadījumā pieliek ++
         if (letterFill(letter, word, unknown)==0)
             {
                 cout << endl << "Whoops! That letter isn't in there!" << endl;
-                guesses++;
+                punkti++;
             }
         else
             {
                 cout << endl << "Congratz! You found one!" << endl;
             }
-                cout << "You have " << MAX_TRIES - guesses;
-                cout << " mistakes left." << endl;
+                    cout << "You have " << punkti;
+                    cout << "/10 points!" << endl;
 // Pārbaude, vai uzminēts vārds
         if (word==unknown)
         {
-            cout << word << endl;
             cout << "Yeah! You got it!";
+            cout << "Asnwer was" << word;
+            if(punkti < 5){
+                punkti = 0;
+                return 0;}
+            else{
+                punkti -= 5;
+                return 0;}
             break;
         }
 }
-        if(guesses == MAX_TRIES)
+        if(punkti == gameover)
         {
             cout << "Sorry, you lose..." << endl;
             cout << "The word was: " << word << endl;
@@ -59,7 +74,7 @@ string unknown(word.length(),'*'); //aizpildam vārdu ar *
     cin.get();
 return 0;
     }
-
+}
 //Ņem minējumu un slepeno vārdu, aizpilda neuzminētos.
 //Atgriež, cik burti uzminēti
 //Atgriež 0, ja burts jau aizvietots.
@@ -80,6 +95,8 @@ int letterFill (char guess, string secretword, string &guessword)
             matches++;
         }
     }
+    
+
 return matches;
 }
 
